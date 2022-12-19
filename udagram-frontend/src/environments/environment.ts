8f82be848dd635c01af29ff6c2de2eefb1898f48
ToqaMuhammad6 <toqaamuhammad6@gmail.com>
2022-12-19 22:58:25 +0200

    updates

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab60a4305c6e9461db0edf572e07824b-444560191.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
